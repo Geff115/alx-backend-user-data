892ca020838c73d4f3cf729f92670ba23ad7740a
Geff115 <gabrielnoah129@gmail.com>
2024-11-11 21:57:39 +0100

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,15 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+# Get the AUTH_TYPE environment variable
+auth_type = os.getenv('AUTH_TYPE')
+
+# Based on the value of auth_type, create instance of Auth
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -25,14 +34,36 @@ def not_found(error) -> str:
 def not_authorized(error) -> str:
     """ Not authorized handler
     """
-    return jsonify({"error": "Unauthorized"})
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def not_allowed(error) -> str:
     """ Not allowed handler
     """
-    return jsonify({"error": "Forbidden"})
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """A function that will run before each request"""
+    if auth is None:
+        return
+
+    list_paths = ['/api/v1/status/',
+                  '/api/v1/unauthorized/',
+                  '/api/v1/forbidden/'
+                  ]
+    # Checking if request.path is not part of this list
+    if request.path not in list_paths:
+        if not auth.require_auth(request.path, list_paths):
+            return
+
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
