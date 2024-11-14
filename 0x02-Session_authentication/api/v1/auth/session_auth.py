3bbec9dde705c19a7a8315b562ff559e18a919e2
Geff115 <gabrielnoah129@gmail.com>
2024-11-14 12:17:33 +0100

    Update: Session Authentication class added

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Session Authentication class that inherits
+from the Auth class
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Managing sessions for authenticated users
+    """
+
+    def __init__(self):
+        """Initialization method"""
+        pass
