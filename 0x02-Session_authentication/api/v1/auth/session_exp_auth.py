00640dcb6f9f2bc72f6397ff6d54a00587ce5a1e
Geff115 <gabrielnoah129@gmail.com>
2024-11-15 16:05:38 +0100

    Update: Added an expiration date to a session ID

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,30 +13,30 @@ class SessionExpAuth(SessionAuth):
 
     def __init__(self):
         """Initialization method"""
-        try:
-            self.session_duration = int(os.getenv('SESSION_DURATION', 0))
-        except ValueError:
-            self.session_duration = 0
+        self.session_duration = int(os.getenv('SESSION_DURATION'), 0)
 
     def create_session(self, user_id=None):
         """Create session for user"""
+        if not user_id:
+            return None
+
         session_id = super().create_session(user_id)
         if not session_id:
             return None
 
         # Store a session id with user id and created_at
-        session_dict = {
+        session_dictionary = {
             'user_id': user_id,
             'created_at': datetime.now()
         }
-        self.user_id_by_session_id[session_id] = session_dict
+        self.user_id_by_session_id[session_id] = session_dictionary
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """Retrieve user_id for a session if it
         is still valid.
         """
-        if session_id is None:
+        if not session_id:
             return None
 
         # Getting session data
@@ -50,7 +50,7 @@ class SessionExpAuth(SessionAuth):
         if self.session_duration <= 0:
             return user_id
 
-        if created_at is None:
+        if not created_at:
             return None
 
         if datetime.now() > created_at + \
