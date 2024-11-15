fbd038fda71aeefc7a3a61e291a1439880ac490a
Geff115 <gabrielnoah129@gmail.com>
2024-11-15 11:34:49 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -22,10 +22,10 @@ auth_type = os.getenv('AUTH_TYPE')
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == 'basic_auth':
+if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-else:
+if auth_type == 'session_auth'
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,14 +4,19 @@ Session Authentication class that inherits
 from the Auth class
 """
 
-import uuid
-import os
+# import uuid
+# import os
 from api.v1.auth.auth import Auth
-from models.user import User
-from flask import jsonify, request, Response
+# from models.user import User
+# from flask import jsonify, request, Response
 
 
 class SessionAuth(Auth):
     """Managing sessions for authenticated users
     """
-    #user_id_by_session_id = {}
+
+    def __init__(self):
+        """
+        Initialization method
+        """
+        pass
