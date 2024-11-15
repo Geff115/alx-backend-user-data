7a6c218f0fdcd1ea06af069bb2ea75954f5bfb91
Geff115 <gabrielnoah129@gmail.com>
2024-11-15 11:39:40 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,11 +4,7 @@ Session Authentication class that inherits
 from the Auth class
 """
 
-# import uuid
-# import os
 from api.v1.auth.auth import Auth
-# from models.user import User
-# from flask import jsonify, request, Response
 
 
 class SessionAuth(Auth):
