80d2c91cafbbfcb6712712b78be69b196324ba99
Geff115 <gabrielnoah129@gmail.com>
2024-11-18 15:45:51 +0100

    User Model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,7 +17,7 @@ class User(Base):
     """User model mapping the users table"""
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
