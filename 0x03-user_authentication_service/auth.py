bd719668759df9448536cabd6fa89b689a121a59
Geff115 <gabrielnoah129@gmail.com>
2024-11-21 14:11:47 +0100

    Update: added a function to find user by session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,14 +66,14 @@ class Auth:
 
         return session_id
 
-    def get_user_from_session_id(self,
-                                 session_id: str) -> Optional[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
         """Geetting a user based on session id"""
         if not session_id:
             return None
 
-        user = self._db.find_user_by(session_id=session_id)
-        if not user:
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
             return None
 
         return user
