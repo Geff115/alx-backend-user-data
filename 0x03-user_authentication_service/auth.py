88769b14c6dce00caf59a43c8052930698526c26
Geff115 <gabrielnoah129@gmail.com>
2024-11-21 12:22:43 +0100

    Update: implemented a method that validates user's login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,6 +39,15 @@ class Auth:
 
         return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validating a user's login credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
 
 def _hash_password(password: str) -> bytes:
     """Hashing password with bcrypt
